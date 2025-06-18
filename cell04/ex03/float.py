1d4a305d5dc156da9732d2ecd2f1f8447d879485
jpaenbor <jiratchaya13890@gmail.com>
2025-06-18 05:47:22 +0000

    04

--- cell04/ex03/float.py
+++ cell04/ex03/float.py
@@ -1,5 +1,5 @@
 num = float(input("Give me a number: "))
 if int(num) == num:
-    print("This number is an integer")
+    print("This number is an integer")  # แสดงผลว่าเป็นจำนวนเต็ม
 else:
-    print("This number is a decimal")
+    print("This number is a decimal")   # แสดงผลว่าเป็นจำนวนทศนิยม
