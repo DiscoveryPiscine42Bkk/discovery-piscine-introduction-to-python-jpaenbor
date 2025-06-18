47d374978fe381fec4373fb10d4619d7632f73cf
jpaenbor <jiratchaya13890@gmail.com>
2025-06-18 04:05:37 +0000

    04

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,16 +1,11 @@
 num1 = int(input("Give me the first number: "))
 num2 = int(input("Give me the second number: "))
-
-addition = num1 + num2
-subtraction = num1 - num2
-multiplication = num1 * num2
-
-if num2 != 0:
-    division = num1 / num2
+print('Thank You')
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+div = num1 / num2
+if div == int(div):
+      print(f"{num1} / {num2} = {int(num1 / num2)}")
 else:
-    division = "undefined (cannot divide by zero)"
-    
-    print(f"{num1} + {num2} = {num1 + num2}")
-    print(f"{num1} - {num2} = {num1 - num2}")
-    print(f"{num1} * {num2} = {num1 * num2}")
-    print(f"{num1} / {num2} = {div}")
+       print(f"{num1} / {num2} = {div}")
+       print(f"{num1} * {num2} = {num1 * num2}")
