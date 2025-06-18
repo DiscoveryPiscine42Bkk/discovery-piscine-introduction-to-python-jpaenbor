e0c38160323485f212f144520e54dccf7183bde0
jpaenbor <jiratchaya13890@gmail.com>
2025-06-18 17:13:50 +0000

    5

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,7 @@
+# play_with_arrays.py
+
+original_numbers = [1, 5, 10, 15, 20]
+new_numbers = [num + 2 for num in original_numbers]
+
+print("Array เดิม:", original_numbers)
+print("Array ใหม่ (แต่ละตัวบวก 2):", new_numbers)
