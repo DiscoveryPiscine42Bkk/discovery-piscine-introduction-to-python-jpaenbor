d5a520e37e0d73d9b7e2041794a91bb942c8e709
jpaenbor <jiratchaya13890@gmail.com>
2025-06-18 17:19:24 +0000

    5

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,5 @@
+original_numbers = [1, 5, 10, 15, 20, 5, 10, 25] # Added some duplicates for testing
+unique_numbers = list(set(original_numbers)) # This is the magic!
+
+print("Original Array:", original_numbers)
+print("Unique Numbers Array:", unique_numbers)
